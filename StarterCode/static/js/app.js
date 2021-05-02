ebfdce5047be0f54cf1de63ae0b58cc311b27a55
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-05-02 09:12:20 -0400

    commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,33 +1,52 @@
-function buildMetadata(sample) {
+//populate dropdown
+function dropdown(){
+    var ID = d3.select("#selDataset");
+    d3.json("samples.json").then((data)=>{
+        var id = data.names;
+        id.forEach((sampleid)=>{
+            ID.append("option")
+            .text(sampleid)
+            .property("value",sampleid)
+        })
 
-    // metadata 
+    })
+}
+
+dropdown()
+
+
+
+
+
+// function buildMetadata(sample) {
+
+//     // metadata 
   
    
-    var metadataURL = `/data/${sample}`;
-      d3.json(metadataURL).then(function(sample){
-        var sampleData = d3.select(`#sample`);
-        sampleData.html("");
-        Object.entries(sample).forEach(function([key,value]){
-          var row = sampleData.append("p");
-          row.text(`${key}:${value}`)
-        })
-      });
-  }
-  function buildCharts(sample) {
-    // Use d3.json to get data
-    var plotData = `/data/${sample}`;
+//       d3.json("samples.json").then(function(sample){
+//         var sampleData = d3.select(`#sample`);
+//         sampleData.html("");
+//         Object.entries(sample).forEach(function([key,value]){
+//           var row = sampleData.append("p");
+//           row.text(`${key}:${value}`)
+//         })
+//       });
+//   }
+//   function buildCharts(sample) {
+//     // Use d3.json to get data
+//     var plotData = `/data/${sample}`;
 
-     // Barchart / horizontal 
-     d3.json(plotData).then(function(data){
-        var values = data.sample_values.slice(0,10);
-        var labels = data.otu_ids.slice(0,10);
-        var display = data.otu_labels.slice(0,10);
+//      // Barchart / horizontal 
+//      d3.json(plotData).then(function(data){
+//         var values = data.sample_values.slice(0,10);
+//         var labels = data.otu_ids.slice(0,10);
+//         var display = data.otu_labels.slice(0,10);
   
-        var barchart = [{
-          values: values,
-          lables: labels,
-          hovertext: display,
-          type: "bar"
-        }];
-        Plotly.newPlot('bar',barchart);
-    });
+//         var barchart = [{
+//           values: values,
+//           lables: labels,
+//           hovertext: display,
+//           type: "bar"
+//         }];
+//         Plotly.newPlot('bar',barchart);
+//     });
